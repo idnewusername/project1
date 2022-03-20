e538625b1bc23855e5d27dfd4faf2e8fbb760f8b
idnewusername <forlibraries@yandex.ru>
2022-03-21 02:38:12 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/aantonov/simple_robot/src
+# Build directory: /home/aantonov/simple_robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("second_robot_simple_control")
+subdirs("second_robot")
---
+++ build/second_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aantonov/simple_robot/src/second_robot
+# Build directory: /home/aantonov/simple_robot/build/second_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
